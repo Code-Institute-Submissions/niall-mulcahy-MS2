72ccf40d83e3d2a5877c0bdd3428af538cf1a2c3
niall-mulcahy <niall-mulcahy@outlook.com>
2021-03-12 11:41:58 +0000

    fixed shapes and added a footer

---
+++ myscripts.js
@@ -0,0 +1,30 @@
+let square = $(".css-shapes-square")
+let triangle = $(".css-shapes-triangle")
+let circle = $(".css-shapes-circle")
+let trapezoid = $(".css-shapes-trapezoid")
+let bookmark = $(".css-shapes-bookmark")
+
+let shapes = [square, triangle, circle, trapezoid, bookmark]
+
+function generateShapeArray(){
+    $("#start-button").click(function() {
+        
+    })
+}
+
+function displayArray() {
+
+}
+
+function generateAnswerArray() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function correctAnswerCounter() {
+
+}
+
