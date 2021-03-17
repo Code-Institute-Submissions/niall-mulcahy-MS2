5db76f79155c41a0e674228a74f2d7115de9cffa
niall-mulcahy <niall-mulcahy@outlook.com>
2021-03-17 13:24:20 +0000

    added shape display functionality

--- myscripts.js
+++ myscripts.js
@@ -4,22 +4,27 @@ let circle = $(".css-shapes-circle")
 let trapezoid = $(".css-shapes-trapezoid")
 let bookmark = $(".css-shapes-bookmark")
 
-let shapes = ["square", "triangle", "circle", "trapezoid", "bookmark"];
+let shapes = [square, triangle, circle, trapezoid, bookmark];
 
 function generateQuestionArray () {
-    var newArray = [];
+    newArray = [];
     for (let i = 0; i < 3; i++){
         newArray.push(shapes[Math.floor(Math.random() * shapes.length)]);
     }
-    console.log(newArray)
+    console.log(newArray);
+
+    for (let i = 0; i < 3; i++) {
+        setTimeout(function () {
+            newArray[i].clone().appendTo(".shape-display-box");
+        }, 1000);
+    };
 };
 
 $("#start-button").click(generateQuestionArray);
 
-
-function displayArray() {
-
-}
+$("button.copy").click(function() {
+    $(this).prev().children("img").clone().appendTo("#copiedimages");
+});
 
 function generateAnswerArray() {
 
