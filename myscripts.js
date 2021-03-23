f08dd8192d94a5df4c295d5a8329e507a58ee331
niall-mulcahy <niall-mulcahy@outlook.com>
2021-03-23 14:31:28 +0000

    changes to check answer function

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,15 @@
-let square = $(".css-shapes-square")
-let triangle = $(".css-shapes-triangle")
-let circle = $(".css-shapes-circle")
-let trapezoid = $(".css-shapes-trapezoid")
-let bookmark = $(".css-shapes-bookmark")
+let questionArray = [];
+let answerArray = [];
+
+let square = $(".css-shapes-square");
+let triangle = $(".css-shapes-triangle");
+let circle = $(".css-shapes-circle");
+let trapezoid = $(".css-shapes-trapezoid");
+let bookmark = $(".css-shapes-bookmark");
 
 let shapes = [square, triangle, circle, trapezoid, bookmark];
 
 function generateQuestionArray () {
-    questionArray = [];
     for (let i = 0; i < 3; i++){
         questionArray.push(shapes[Math.floor(Math.random() * shapes.length)]);
     }
@@ -22,47 +24,40 @@ function generateQuestionArray () {
 }
 
 $("#start-button").click(generateQuestionArray);
-
 $("#submit-button").click(checkAnswer);
 
-var answerArray = [];
 
-square.click(function() {
-    answerArray.push(square);
-    console.log(answerArray);
-});
+    square.click(function() {
+        answerArray.push(square);
+        console.log(answerArray);
+    });
 
-triangle.click(function() {
-    answerArray.push(triangle);
-    console.log(answerArray);
-});
+    triangle.click(function() {
+        answerArray.push(triangle);
+        console.log(answerArray);
+    });
 
-circle.click(function() {
-    answerArray.push(circle);
-    console.log(answerArray);
-});
+    circle.click(function() {
+        answerArray.push(circle);
+        console.log(answerArray);
+    });
 
-trapezoid.click(function() {
-    answerArray.push(trapezoid);
-    console.log(answerArray);
-});
+    trapezoid.click(function() {
+        answerArray.push(trapezoid);
+        console.log(answerArray);
+    });
 
-bookmark.click(function() {
-    answerArray.push(bookmark);
-    console.log(answerArray);
-});
+    bookmark.click(function() {
+        answerArray.push(bookmark);
+        console.log(answerArray);
+    });
 
-function checkAnswer(questionArray, answerArray) {
-    if (JSON.stringify(questionArray) === JSON.stringify(answerArray)) {
-        alert ("You got it right!");}
-        else {
-            alert ("nice try! Better luck next time!");
-        }
-    questionArray = [];
+function checkAnswer(shapes){
+    console.log("hello my name is human");
+    answerArray = [];
+    console.log(answerArray)
 }
-
 
 function correctAnswerCounter() {
 
 }
-
